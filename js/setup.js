79ebbaae7ca3b9392f369b26be39a82147801a84
SatrikTS <satrik90@gmail.com>
2017-02-02 23:02:35 +0300

    Четвертый модуль. ARIA, role, keydown events

--- js/setup.js
+++ js/setup.js
@@ -1,16 +1,87 @@
 'use strict';
 // Открытие и закрытие всплывающего окна;
-var setupOpen = document.querySelector('.setup-open');
-var showSetup = document.querySelector('.setup');
-var setupClose = document.querySelector('.setup-close');
+var setupOpen = document.querySelector('.setup-open img');
+var setupDialog = document.querySelector('.setup');
+var setupClose = setupDialog.querySelector('.setup-close');
+var setupSubmitDialog = document.querySelector('.setup-submit');
+var CODE_ENTER_KEY = 13;
+var CODE_ESC_KEY = 27;
 
+var isActivateEvent = function (event) {
+   return event.keyCode && event.keyCode === CODE_ENTER_KEY;
+}
+
+var setupKeydownHandler = function (event) {
+  if (event.keyCode === CODE_ESC_KEY){
+      closeDialog();
+  }
+}
+
+var showDialog = function () {
+  setupDialog.classList.remove('invisible');
+  document.addEventListener('keydown', setupKeydownHandler);
+}
+
+var closeDialog = function () {
+  setupDialog.classList.add('invisible');
+  document.removeEventListener('keydown', setupKeydownHandler);
+}
+
+// Открытие и закрытие диалогового окна по клику
 setupOpen.addEventListener('click', function () {
-  showSetup.classList.remove('invisible');
+  showDialog();
+  handleBtnClick(event)
 });
 
 setupClose.addEventListener('click', function () {
-  showSetup.classList.add('invisible');
+  closeDialog();
+  handleBtnClick(event)
 });
+
+// Открытие и закрытие диалогового окна по событиям клавиатуры
+setupOpen.addEventListener('keydown', function (event) {
+  if (isActivateEvent(event)){
+    showDialog();
+    handleBtnClick(event)
+  }
+});
+
+setupClose.addEventListener('keydown', function (event){
+  if (isActivateEvent(event)){
+      closeDialog();
+      handleBtnClick(event)
+    }
+});
+
+setupSubmitDialog.addEventListener('click', function (event) {
+  event.preventDefault();
+  closeDialog();
+  handleBtnClick(event)
+});
+
+setupSubmitDialog.addEventListener('keydown', function (event) {
+  event.preventDefault();
+  if (isActivateEvent(event)){
+    closeDialog();
+    handleBtnClick(event)
+  }
+});
+
+// Обновление атрибута aria-pressed
+function handleBtnClick(event) {
+  toggleButton(event.target);
+}
+
+function handleBtnKeyPress(event) {
+  if (isActivateEvent(event)) {
+    toggleButton(event.target);
+  }
+}
+
+function toggleButton(element) {
+  var pressed = (element.getAttribute("aria-pressed") === "true");
+  element.setAttribute("aria-pressed", !pressed);
+}
 
 // Изменение цвета куртки у волшебника;
 var COLOR_COAT_ARR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
