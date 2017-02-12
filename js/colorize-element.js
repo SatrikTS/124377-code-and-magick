cb98903ba82def0e9bafec04e72a265e7ecc6edd
SatrikTS <satrik90@gmail.com>
2017-02-12 18:59:12 +0300

    Внесены правки, по цветам. Правка eslint

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,16 +1,14 @@
 'use strict';
-
+var CODE_ENTER_KEY = 13;
 window.colorizeElement = function (element, colors, property) {
   element.addEventListener('click', function () {
     var currentColor = element.style[property];
     element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
-    console.log(element.style[property] = window.utils.getRandomElementExcept(colors, currentColor));
   });
   element.addEventListener('keydown', function () {
     var currentColor = element.style[property];
-    if (isActivateEvent(event)){
+    if (event.keyCode === CODE_ENTER_KEY) {
       element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
-      console.log(element.style[property] = window.utils.getRandomElementExcept(colors, currentColor));
     }
   });
 };
