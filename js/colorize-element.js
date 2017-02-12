cf5a506fc5a44121ea14c81d0f06c8f862fa81cf
SatrikTS <satrik90@gmail.com>
2017-02-12 13:35:24 +0300

    Внесены правки, по цветам

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -2,11 +2,15 @@
 
 window.colorizeElement = function (element, colors, property) {
   element.addEventListener('click', function () {
-    var CurrentColor = null;
-    element.style[property] = window.utils.getRandomElementExcept(colors, CurrentColor);
+    var currentColor = element.style[property];
+    element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
+    console.log(element.style[property] = window.utils.getRandomElementExcept(colors, currentColor));
   });
   element.addEventListener('keydown', function () {
-    var CurrentColor = null;
-    element.style[property] = window.utils.getRandomElementExcept(colors, CurrentColor);
+    var currentColor = element.style[property];
+    if (isActivateEvent(event)){
+      element.style[property] = window.utils.getRandomElementExcept(colors, currentColor);
+      console.log(element.style[property] = window.utils.getRandomElementExcept(colors, currentColor));
+    }
   });
 };
--- js/setup.js
+++ js/setup.js
@@ -87,10 +87,15 @@ var wizardEyes = document.querySelector('#wizard-eyes');
 var wizardCoat = document.querySelector('#wizard-coat');
 var fireballColor = document.querySelector('.setup-fireball-wrap');
 
+var COLOR_EYES_ARR = ['black', 'red', 'blue', 'yellow', 'green'];
+var COLOR_FIREBALL_ARR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+var COLOR_COAT_ARR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+
 wizardEyes.setAttribute('tabindex', '0');
 wizardCoat.setAttribute('tabindex', '0');
 fireballColor.setAttribute('tabindex', '0');
 
-window.colorizeElement(wizardEyes, ['black', 'red', 'blue', 'yellow', 'green'], 'fill');
-window.colorizeElement(wizardCoat, ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'], 'fill');
-window.colorizeElement(fireballColor, ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'], 'background');
+window.colorizeElement(wizardEyes, COLOR_EYES_ARR, 'fill');
+window.colorizeElement(wizardCoat, COLOR_COAT_ARR, 'fill');
+window.colorizeElement(fireballColor, COLOR_FIREBALL_ARR, 'background');
